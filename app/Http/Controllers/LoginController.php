2c2048185807290f4229ab97dfd2eae1f3eb8db8
N4Muadman <126045945+N4Muadman@users.noreply.github.com>
2025-06-02 15:44:37 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\attendance;
@@ -11,33 +12,41 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function index(){
-        if(Auth::check()){
+    public function index()
+    {
+        if (Auth::check()) {
             return redirect()->route('home.admin');
         }
 
         return view('login.index');
     }
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $credentials = $request->only('username', 'password');
         if (Auth::attempt($credentials, true)) {
+            if (Auth::user()->employee->end_date) {
+                Auth::logout();
+                return redirect()->back()->with(['errorLogin' => 'Tên đăng nhập hoặc mật khẩu sai']);
+            }
 
             Cache::forever('user_permissions_' . Auth::user()->id, Auth::user()->role->pagePermissions()->where('status', 1)->get(['permission_id', 'page_id'])->toArray());
 
             return redirect()->route('home.admin')->with('success', 'Đăng nhập thành công');
         }
 
-        return redirect()->back()->with(['errorLogin'=>'Tên đăng nhập hoặc mật khẩu sai']);
+        return redirect()->back()->with(['errorLogin' => 'Tên đăng nhập hoặc mật khẩu sai']);
     }
-    public function logout(){
-        if (Auth::check()){
+    public function logout()
+    {
+        if (Auth::check()) {
             Cache::forget('user_permissions_' . Auth::user()->id);
             Auth::logout();
         }
         return redirect()->route('login.index');
     }
 
-    public function download(){
+    public function download()
+    {
         return response()->download(storage_path('app/employee-must-read.pdf'));
     }
 }
